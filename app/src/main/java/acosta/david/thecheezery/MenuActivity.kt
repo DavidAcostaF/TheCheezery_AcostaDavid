7fefd755c21095dab0b7cba29b6929a7e219d392
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2025-02-11 21:08:32 -0700

    Add Product data class and update layout files for improved UI

--- app/src/main/java/acosta/david/thecheezery/MenuActivity.kt
+++ app/src/main/java/acosta/david/thecheezery/MenuActivity.kt
@@ -1,6 +1,9 @@
 package acosta.david.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,29 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+        var btnHot: Button = findViewById(R.id.button_hot_drinks) as Button
+        var btnSweets: Button = findViewById(R.id.button_sweets) as Button
+        var btnSalties: Button = findViewById(R.id.button_salties) as Button
+
+        btnCold.setOnClickListener {
+            viewEvent("cold")
+        }
+        btnHot.setOnClickListener {
+            viewEvent("hot")
+        }
+        btnSweets.setOnClickListener {
+            viewEvent("sweets")
+        }
+        btnSalties.setOnClickListener {
+            viewEvent("salties")
+        }
+
+    }
+    fun viewEvent(type:String){
+        val intent = Intent(this@MenuActivity, ProductosActivity::class.java)
+        intent.putExtra("type", type)
+        startActivity(intent)
     }
 }
